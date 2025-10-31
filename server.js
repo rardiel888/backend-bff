faf3de12d706aabed4d571af8de6967e88de968a
Rardiel <salasceballorardiel@gmail.com>
2025-10-31 12:58:33 -0400

    Hola funciona postman

--- server.js
+++ server.js
@@ -71,5 +71,10 @@ app.listen(PORT, async () => {
   } catch (error) {
     console.error('❌ No se pudo conectar a PostgreSQL:', error);
   }
+
+  app.get('/api/hola', (req, res) => {
+  res.json({ mensaje: 'Hola, aquí funciona 🎉' });
+});
+
 });
 
